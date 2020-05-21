cb479c95d70adbe3dc9918794063b306f99724ab
code678-code <QKcode@163.com>
2020-05-21 14:51:17 +0800

    修改

--- codemain/src/test/com/maintest.java
+++ codemain/src/test/com/maintest.java
@@ -3,10 +3,11 @@ package test.com;
 public class maintest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaa");
-
+        //aaaaaaaaaaaaaaaaa
     }
     public void test(){
         System.out.println("bbbbbbbbbbbbbbbb");
+        //bbbbbbbbbbbbbbbb
 
     }
 }
