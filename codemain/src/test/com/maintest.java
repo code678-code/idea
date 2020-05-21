4196142b60dfc89697aa9790e01fc77a137ffa1e
code678-code <QKcode@163.com>
2020-05-21 16:27:41 +0800

    提交

--- codemain/src/test/com/maintest.java
+++ codemain/src/test/com/maintest.java
@@ -4,6 +4,7 @@ public class maintest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaa");
         //aaaaaaaaaaaaaaaaa
+        System.out.println("cccccccccccccccccccccc");//增加c
     }
     public void test(){
         System.out.println("bbbbbbbbbbbbbbbb");
---
+++ test/src/test/com/test.java
@@ -0,0 +1,7 @@
+package test.com;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试");
+    }
+}
